6019a1d68964daf86d8d5f3a36d72679985bc3ff
Het <62541263+HETFADIA@users.noreply.github.com>
2021-05-28 15:14:51 +0530

    Added time calculator

--- main.js
+++ main.js
@@ -28,3 +28,23 @@ function generate(){
     copyText = document.getElementById("myInput");
     console.log(copyText);
 }
+function time(input,output,loop,inputspeed,outputspeed,loopspeed){
+    return input/inputspeed+output/outputspeed+loop/loopspeed;
+}
+function pow(a,b){
+    return Math.pow(a,b);
+}
+function calculate(){
+    var input = +document.getElementById("inputSize").value;
+    var output=+document.getElementById("outputSize").value;
+    var loop=+document.getElementById("loop").value;
+    console.log(loop)
+    var tcpp=time(input,output,loop,5*1e6,1e6,5*1e8).toFixed(2)
+    var tpy=time(input,output,loop,1e6,5*1e5,1e7).toFixed(2)
+    var tpybig=(tpy*5).toFixed(2)
+    var tpypy=time(input,output,loop,1e6,2*1e5,1e8).toFixed(2)
+    document.getElementById("time").innerHTML="Time Taken for cpp(fastio): "+tcpp+"s"+"<br>";
+    document.getElementById("time").innerHTML+="Time Taken for python(fastio): "+tpy+"s"+"<br>";
+    document.getElementById("time").innerHTML+="Time Taken for pypy(bigint and fastio): "+tpypy*5+"s"+"<br>";
+    document.getElementById("time").innerHTML+="Time Taken for pypy(fastio): "+tpypy+"s"+"<br>";
+}
