edfdea0e2d80a7066406b1d159b27982f6166a64
Het <62541263+HETFADIA@users.noreply.github.com>
2021-07-11 04:13:06 +0530

    update permutations

--- js/main.js
+++ js/main.js
@@ -253,33 +253,17 @@ function* charCombinations (chars, minLength, maxLength) {
     chars = typeof chars === 'string' ? chars : '';
     minLength = parseInt(minLength) || 0;
     maxLength = Math.max(parseInt(maxLength) || 0, minLength);
-    
-    //Generate for each word length
     for (i = minLength; i <= maxLength; i++) {
-  
-      //Generate the first word for the password length by the repetition of first character.
       word = (chars[0] || '').repeat(i);
       yield word;
-  
-      //Generate other possible combinations for the word
-      //Total combinations will be chars.length raised to power of word.length
-      //Make iteration for all possible combinations
       for (j = 1; j < Math.pow(chars.length, i); j++) {
-  
-        //Make iteration for all indices of the word
         for(k = 0; k < i; k++) {
-  
-          //check if the current index char need to be flipped to the next char.
           if(!(j % Math.pow(chars.length, k))) {
-            
-            // Flip the current index char to the next.
             let charIndex = chars.indexOf(word[k]) + 1;
             char = chars[charIndex < chars.length ? charIndex : 0];
             word = word.substr(0, k) + char + word.substr(k + char.length);
           }
         }
-  
-        //Re-oder not neccesary but it makes the words are yeilded alphabetically on ascending order.
         yield word.split('').reverse().join('');
       }
     }
@@ -639,12 +623,22 @@ function generatePermutation(){
     var n=evaladv(document.getElementById("permutation").value);
     Permutation(n);
 }
+function exists(a,b){
+    for(var i of a){
+        if(i==b){
+            return 1;
+        }
+    }
+    return 0;
+}
 function ArrayPermutation(){
     var array=document.getElementById("arrayPermutation").value
+    if(exists(array," ")){
+        array=split(array)
+    }
     var min=document.getElementById("minValuePermutation").value;
     var max=document.getElementById("maxValuePermutation").value;
-    console.log(array,min,max)
-    console.log(permutations(array,min,max))
+    
     document.getElementById("outArrayPermutation").innerHTML=permutations(array,min,max,"")
 }
 function isPrime(n){
